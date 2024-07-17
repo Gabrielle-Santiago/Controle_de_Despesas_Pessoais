67fa05f9916a6ffe043dd9e81f8936a6177efb91
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2024-07-17 19:19:49 -0300

    Criação da função tela de cadastro

--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -5,4 +5,6 @@ from task import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login, name='login'),
 ]
--- setup/task/views.py
+++ setup/task/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Home do projeto
 def home(request):
     return render(request, 'home.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def login(request):
+    return render(request, 'login.html')
