ca2feebc4379b764a5f76496e738561d3b5e562d
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2024-07-18 11:10:13 -0300

    Verificação do cadastro (senhas iguais e usuário existente)

--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login'),
+    path('login/', views.Login, name='login'),
 ]
--- setup/task/views.py
+++ setup/task/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Home do projeto
 def home(request):
     return render(request, 'home.html')
 
-def cadastro(request):
-    return render(request, 'cadastro.html')
 
-def login(request):
+def cadastro(request):
+    
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], email=request.POST['email'], password=request.POST['password1'])
+                user.last_name = request.POST['last_name']
+                user.save()
+
+                login(request, user)
+                return redirect('home')
+
+            except:
+                return render(request, 'cadastro.html',
+                              {
+                              'form': UserCreationForm,
+                              'error': 'Usuário já existe!'
+                              } )
+                              
+        return render(request, 'cadastro.html',
+                              {
+                              'form': UserCreationForm,
+                              'error': 'Senhas não são iguais!!'
+                              })
+
+
+def Login(request):
+
     return render(request, 'login.html')
