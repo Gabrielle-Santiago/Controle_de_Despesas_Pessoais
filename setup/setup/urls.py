e746014e533b6091c85792ec40d9a340247d5ff7
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2024-07-22 10:08:38 -0300

    Envio e recebimento das informações cadastrais

--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.Login, name='login'),
+    path('tasks/', views.tasks, name='tasks'),
 ]
--- setup/task/views.py
+++ setup/task/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 
-# Home do projeto
 def home(request):
     return render(request, 'home.html')
 
@@ -14,29 +13,59 @@ def cadastro(request):
         return render(request, 'cadastro.html')
     
     else:
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        last_name = request.POST['last_name']
+        
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(username=request.POST['username'], email=request.POST['email'], password=request.POST['password1'])
+                
                 user.last_name = request.POST['last_name']
                 user.save()
 
                 login(request, user)
                 return redirect('home')
 
-            except:
+            except User.DoesNotExist:
                 return render(request, 'cadastro.html',
                               {
-                              'form': UserCreationForm,
+                              'form': UserCreationForm(),
                               'error': 'Usuário já existe!'
                               } )
                               
         return render(request, 'cadastro.html',
                               {
-                              'form': UserCreationForm,
+                              'form': UserCreationForm(),
                               'error': 'Senhas não são iguais!!'
                               })
 
 
 def Login(request):
 
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html',
+                              {
+                              'form': AuthenticationForm
+                              } )
+    
+    else:
+        user = authenticate(
+
+        request, email=request.POST['email'], password=request.POST['password'])
+
+        if user is None:
+            return render(request, 'login.html', {
+
+                'form' : AuthenticationForm,
+                'error' : 'Email ou senhas estão incorretos!!'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+        
+        
+def tasks(request):
+    return render(request, 'tasks.html')
